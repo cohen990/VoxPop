e222523936c1b2bc480a59785534aa22487b1870
cohen990 <dan.cohen@mopowered.co.uk>
2014-12-21 19:44:23 +0000

    Initial commit + some table storage stuff

---
+++ VoxPop/Site/Models/BlogViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Site.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using Services;
+
+    public class BlogViewModel
+    {
+        public string Title { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Content { get; set; }
+
+        public List<string> PollOptions { get; set; }
+
+        public BlogEntity AsEntity()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ VoxPop/Site/Services/IBlogService.cs
@@ -0,0 +1,12 @@
+﻿namespace Site.Services
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Models;
+
+    public interface IBlogService
+    {
+        void CreateAsync(BlogViewModel blog);
+        Task<IEnumerable<BlogEntity>> GetAllAsync();
+    }
+}
