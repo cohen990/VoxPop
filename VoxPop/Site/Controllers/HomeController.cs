159dcd00272ba7c52476464bcbb757d4d1df25e1
AJizzleBizzle <aaron_jerez@hotmail.com>
2015-01-27 16:55:15 +0000

    Deleted a redundant action 'Vote2' in the HomeController that I copy and pasted in the last commit.

--- VoxPop/Site/Controllers/HomeController.cs
+++ VoxPop/Site/Controllers/HomeController.cs
@@ -65,20 +65,6 @@
             return RedirectToAction("Index");
         }
     
-        public ActionResult Vote2(string pollItemKey, string blogPostPartitionKey, string blogPostRowKey)
-        {
-            var model = new VoteModel
-            {
-                PollItemKey = pollItemKey,
-                BlogPostPartitionKey = blogPostPartitionKey,
-                BlogPostRowKey = blogPostRowKey
-            };
-
-            _blogService.Vote(model);
-
-            return RedirectToAction("Index");
-
-        }
     
     }
 
