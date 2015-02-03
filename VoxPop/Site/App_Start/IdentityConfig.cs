18ebb543a1e7da5e70d19d61f1ef395611b758ea
Samantha Cohen <samanthacohen294@gmail.com>
2015-02-03 21:23:33 +0000

    got it working

--- VoxPop/Site/App_Start/IdentityConfig.cs
+++ VoxPop/Site/App_Start/IdentityConfig.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using System.Web;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin;
-using Microsoft.Owin.Security;
-using VoxPop.Models;
-
-namespace VoxPop
+﻿namespace Site
 {
+    using System;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.Owin;
+    using Microsoft.Owin;
+    using Microsoft.Owin.Security;
+    using Models;
+
     public class EmailService : IIdentityMessageService
     {
         public Task SendAsync(IdentityMessage message)
@@ -33,18 +28,18 @@ namespace VoxPop
     }
 
     // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
-    public class ApplicationUserManager : UserManager<ApplicationUser>
+    public class ApplicationUserManager : UserManager<Politico>
     {
-        public ApplicationUserManager(IUserStore<ApplicationUser> store)
+        public ApplicationUserManager(IUserStore<Politico> store)
             : base(store)
         {
         }
 
         public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
         {
-            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
+            var manager = new ApplicationUserManager(new ElCamino.AspNet.Identity.AzureTable.UserStore<Politico>(context.Get<AuthenticationContext>()));
             // Configure validation logic for usernames
-            manager.UserValidator = new UserValidator<ApplicationUser>(manager)
+            manager.UserValidator = new UserValidator<Politico>(manager)
             {
                 AllowOnlyAlphanumericUserNames = false,
                 RequireUniqueEmail = true
@@ -67,11 +62,11 @@ namespace VoxPop
 
             // Register two factor authentication providers. This application uses Phone and Emails as a step of receiving a code for verifying the user
             // You can write your own provider and plug it in here.
-            manager.RegisterTwoFactorProvider("Phone Code", new PhoneNumberTokenProvider<ApplicationUser>
+            manager.RegisterTwoFactorProvider("Phone Code", new PhoneNumberTokenProvider<Politico>
             {
                 MessageFormat = "Your security code is {0}"
             });
-            manager.RegisterTwoFactorProvider("Email Code", new EmailTokenProvider<ApplicationUser>
+            manager.RegisterTwoFactorProvider("Email Code", new EmailTokenProvider<Politico>
             {
                 Subject = "Security Code",
                 BodyFormat = "Your security code is {0}"
@@ -81,22 +76,22 @@ namespace VoxPop
             var dataProtectionProvider = options.DataProtectionProvider;
             if (dataProtectionProvider != null)
             {
-                manager.UserTokenProvider = 
-                    new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"));
+                manager.UserTokenProvider =
+                    new DataProtectorTokenProvider<Politico>(dataProtectionProvider.Create("ASP.NET Identity"));
             }
             return manager;
         }
     }
 
     // Configure the application sign-in manager which is used in this application.
-    public class ApplicationSignInManager : SignInManager<ApplicationUser, string>
+    public class ApplicationSignInManager : SignInManager<Politico, string>
     {
         public ApplicationSignInManager(ApplicationUserManager userManager, IAuthenticationManager authenticationManager)
             : base(userManager, authenticationManager)
         {
         }
 
-        public override Task<ClaimsIdentity> CreateUserIdentityAsync(ApplicationUser user)
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync(Politico user)
         {
             return user.GenerateUserIdentityAsync((ApplicationUserManager)UserManager);
         }
---
+++ VoxPop/Site/Models/Politico.cs
@@ -0,0 +1,29 @@
+﻿namespace Site.Models
+{
+    using System;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+    using ElCamino.AspNet.Identity.AzureTable.Model;
+    using Microsoft.AspNet.Identity;
+
+    public class Politico : IdentityUser
+    {
+        /// <summary>
+        /// Generated the <see cref="ClaimsIdentity"/> of this <see cref="Retailer"/>.
+        /// </summary>
+        /// <param name="manager">The <see cref="UserManager{T}"/> or <see cref="Retailer"/> entities.</param>
+        /// <returns>The <see cref="ClaimsIdentity"/>.</returns>
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<Politico> manager)
+        {
+            if (manager == null)
+                throw new ArgumentNullException("manager");
+
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            ClaimsIdentity userIdentity =
+                await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+}
