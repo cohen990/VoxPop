0870631135404bf97dc5bcc9a93a460f4877aba8
Samantha Cohen <samanthacohen294@gmail.com>
2015-02-24 21:32:25 +0000

    encoding and decoding poll options - Added PollService to handle encoding and decoding (allows us to swap out the encoding/decoding for a different method in the future more easily) - added unit tests - lots of refactoring (yay) - also fixed the build (after breaking it for the sake of breaking the live site :<)
    closes #41

---
+++ VoxPop/VoxPop.Tests.cs/Models/when_encoding_poll_options_list.cs
@@ -0,0 +1,28 @@
+﻿namespace Site.Models
+{
+    using Machine.Specifications;
+    using System.Collections.Generic;
+    using Services;
+
+    class when_encoding_poll_options_list
+    {
+        internal static List<string> pollOptions;
+        internal static List<string> result;
+
+        private Establish context = () => pollOptions = new List<string>
+        {
+            "option 1",
+            "comma,, 2",
+            "plus + minus - option",
+            "\"escaped quotes\""
+        };
+
+        private Because of = () => result = pollOptions.EncodePollOptions();
+
+        private It should_return_same_number_of_options = () => result.Count.ShouldEqual(pollOptions.Count);
+        private It should_encode_option_1_as_option_plus_1 = () => result.ShouldContain("option+1");
+        private It should_encode_comma_2 = () => result.ShouldContain("comma%2c%2c+2");
+        private It should_encode_plus_minus_option = () => result.ShouldContain("plus+%2b+minus+-+option");
+        private It should_encode_escaped_quotes = () => result.ShouldContain("%22escaped+quotes%22");
+    }
+}
