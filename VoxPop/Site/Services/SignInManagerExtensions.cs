324d2ebb5d33f6c5f963dc2ed1306718f9de0e32
Dan Cohen <dan.cohen@mopowered.co.uk>
2015-03-09 20:19:46 +0000

    Updated username to be constructed from first and last name

---
+++ VoxPop/Site/Services/SignInManagerExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Site.Services
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNet.Identity.Owin;
+    using Storage.Models;
+
+    public static class SignInManagerExtensions
+    {
+        public static async Task<SignInStatus> SignInWithEmailAsync(
+            this SignInManager<Politico, string> manager,
+            string emailAddress,
+            string password,
+            bool isPersistent,
+            bool shouldLockout)
+        {
+            if (manager.UserManager == null)
+            {
+                return SignInStatus.Failure;
+            }
+
+            var user = await manager.UserManager.FindByEmailAsync(emailAddress);
+
+            return await manager.PasswordSignInAsync(user.UserName, password, isPersistent, shouldLockout);
+        }
+    }
+}
