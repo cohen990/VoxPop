98987cc366d0dd8355dd757302526b54d419c61f
Dan Cohen <dan.cohen@mopowered.co.uk>
2015-03-22 15:44:39 +0000

    stripping html tags Now stripping html tags from the content of og:description
    closes #132

---
+++ VoxPop/Site/Services/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Site.Services
+{
+    using System.Text.RegularExpressions;
+
+    public static class StringExtensions
+    {
+        public static string StripHtmlTags(this string content)
+        {
+            const string pattern = @"<(.|\n)*?>";
+
+            return Regex.Replace(content, pattern, string.Empty);;
+        }
+    }
+}
