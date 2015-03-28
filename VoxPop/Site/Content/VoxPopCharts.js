d731bb7bbe213b758f2336a33a8eacaf21828436
AJizzleBizzle <aaron_jerez@hotmail.com>
2015-03-28 03:20:11 +0000

    WIP

--- VoxPop/Site/Content/VoxPopCharts.js
+++ VoxPop/Site/Content/VoxPopCharts.js
@@ -1,9 +1,9 @@
-﻿var VoxPopCharts = (function (){
+﻿var VoxPopCharts = (function () {
     var EmptyChart = [
         {
             value: 1,
-            color: "#d6d6c2",
-            highlight: "#ddd",
+            color: "#ddd",
+            highlight: "#bbb",
             label: "Be the first to vote on this Story"
 
         }
@@ -13,17 +13,17 @@
     //red/blue/yellow/green/purple/orange/red/blue/yellow/green/purple/orange/red/red/red/red etc... (repeating red after 12 colours)
     var colourList = [
         { colour: "#FFd1d1", highlight: "#FF7C8B" },
-        { colour: "#bbdeff", highlight: "#9fd1ff" },
+        { colour: "#bbdeff", highlight: "#88c5ff" },
         { colour: "#fffd9f", highlight: "#FFFB53" },
-        { colour: "#7fff80", highlight: "#5AFF5B" },
+        { colour: "#7fff80", highlight: "#49ff4a" },
         { colour: "#d8b2d8", highlight: "#bf7fbf" },
-        { colour: "#ffc04c", highlight: "#ffb732" },
-        { colour: "#ff1919", highlight: "#ff0000" },
-        { colour: "#3abed7", highlight: "#22b6d2" },
+        { colour: "#ffd27f", highlight: "#ffc04c" },
+        { colour: "#ff2a2a", highlight: "#ff0000" },
+        { colour: "#37c4df", highlight: "#1d9db5" },
         { colour: "#ffff32", highlight: "#ffff00" },
         { colour: "#32ff33", highlight: "#00ff01" },
-        { colour: "#993299", highlight: "#993299" },
-        { colour: "#ffae19", highlight: "#FFA500" },
+        { colour: "#993299", highlight: "#732573" },
+        { colour: "#ffae19", highlight: "#e59400" },
         { colour: "#FFd1d1", highlight: "#FF7C8B" }
     ];
 
@@ -70,7 +70,7 @@
         return result;
     }
 
-    var GetPreparedData = function(data) {
+    var GetPreparedData = function (data) {
 
         var noVotes = true;
 
@@ -97,11 +97,13 @@
         var context = document.getElementById(identifier).getContext("2d");
 
         var myPieChart = new Chart(context).Pie(data, {
+            //responsive: true,
             animationEasing: "easeOutQuart",
             animateScale: true,
-            //segmentShowStroke : true,
-            //segmentStrokeColor: "#040404",
-            //segmentStrokeWidth : 4
+            segmentShowStroke: true,
+            segmentStrokeColor: "#060606",
+            segmentStrokeWidth: 5,
+            legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><div class=\"comm-how\"><%=segments[i].value%>%</div><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
         });
 
     }
@@ -117,4 +119,30 @@
         InitializeChart: InitializeChart,
         GetPreparedData: GetPreparedData
     }
+
+//                       //
+
+    var helpers = Chart.helpers;
+    var legendHolder = document.getElementById('my-doughnut-legend')
+    legendHolder.innerHTML = myPieChart.generateLegend();
+    // Include a html legend template after the module doughnut itself
+    helpers.each(legendHolder.firstChild.childNodes, function(legendNode, index){
+        helpers.addEvent(legendNode, 'mouseover', function(){
+            var activeSegment = myPieChart.segments[index];
+            activeSegment.save();
+            activeSegment.fillColor = activeSegment.highlightColor;
+            myPieChart.showTooltip([activeSegment]);
+            activeSegment.restore();
+        });
+    });
+    helpers.addEvent(legendHolder.firstChild, 'mouseout', function(){
+        myPieChart.draw();
+    });
+    canvas.parentNode.parentNode.appendChild(legendHolder.firstChild);
+
+    myPieChart.generateLegend();
+    document.getElementById('my-doughnut-legend').innerHTML = myPieChart.generateLegend();
+
+//                           //
+
 })();
