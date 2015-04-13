885a06f0785fb46868061b364e04846e576b22b6
AJizzleBizzle <aaron_jerez@hotmail.com>
2015-04-13 04:05:02 +0100

    WIP

---
+++ VoxPop/Site/Scripts/vote-ops.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+
+    for (var i = 0; i < 4; i++) {
+        (function (i) {
+
+            document.getElementById("story-votebuttons-box-" + i).addEventListener("mouseenter", function () {
+                $("#story-votebuttons-box-colour-" + i).addClass("hover").addClass("highlight-" + i);
+            });
+
+            document.getElementById("story-votebuttons-box-" + i).addEventListener("mouseleave", function () {
+                $("#story-votebuttons-box-colour-" + i).removeClass("hover").removeClass("highlight-" + i);
+            });
+        }(i));
+    }
+
+    document.getElementById("story-votebuttons").addEventListener("mouseenter", function () {
+        $(".story-votebuttons-button").addClass("story-votebuttons-button-highlight");
+        $(".story-votebuttons-value").addClass("story-votebuttons-value-highlight");
+        $(".story-votebuttons-option").addClass("story-votebuttons-option-highlight");
+    });
+
+    document.getElementById("story-votebuttons").addEventListener("mouseleave", function () {
+        $(".story-votebuttons-button").removeClass("story-votebuttons-button-highlight");
+        $(".story-votebuttons-value").removeClass("story-votebuttons-value-highlight");
+        $(".story-votebuttons-option").removeClass("story-votebuttons-option-highlight");
+    });
+
+})
