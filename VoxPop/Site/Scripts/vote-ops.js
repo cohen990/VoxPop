0784630fe02f8649ff45420b1897e6a7ad1710c8
AJizzleBizzle <aaron_jerez@hotmail.com>
2015-04-13 22:26:25 +0100

    fixes #178 - nailed it.

--- VoxPop/Site/Scripts/vote-ops.js
+++ VoxPop/Site/Scripts/vote-ops.js
@@ -1,5 +1,6 @@
 ﻿$(document).ready(function () {
 
+/// Stories
     for (var i = 0; i < 4; i++) {
         (function (i) {
 
@@ -25,4 +26,27 @@
         $(".story-votebuttons-option").removeClass("story-votebuttons-option-highlight");
     });
 
+/// Modal
+    for (var i = 0; i < 13; i++) {
+        (function (i) {
+
+            document.getElementById("modal-box-" + i).addEventListener("mouseenter", function () {
+                $("#story-votebuttons-modal-box-colour-" + i).addClass("modal-hover").addClass("modal-highlight-" + i);
+
+                $("#story-votebuttons-modal-button-" + i).addClass("story-votebuttons-modal-button-highlight");
+                $("#modal-value-" + i).addClass("story-votebuttons-modal-value-highlight");
+                $("#story-votebuttons-modal-option-" + i).addClass("story-votebuttons-modal-option-highlight");
+
+            });
+
+            document.getElementById("modal-box-" + i).addEventListener("mouseleave", function () {
+                $("#story-votebuttons-modal-box-colour-" + i).removeClass("modal-hover").removeClass("modal-highlight-" + i);
+
+                $("#story-votebuttons-modal-button-" + i).removeClass("story-votebuttons-modal-button-highlight");
+                $("#modal-value-" + i).removeClass("story-votebuttons-modal-value-highlight");
+                $("#story-votebuttons-modal-option-" + i).removeClass("story-votebuttons-modal-option-highlight");
+
+            });
+        }(i));
+    }
 })
