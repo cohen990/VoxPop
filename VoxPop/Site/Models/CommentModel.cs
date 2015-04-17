81c770d8e14296ff98e75a700a0cfd1821da2d0a
AJizzleBizzle <aaron_jerez@hotmail.com>
2015-04-17 03:01:57 +0100

    Yh, you guessed it - WIP

---
+++ VoxPop/Site/Models/CommentModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Site.Models
+{
+    public class CommentModel
+    {
+        public string UserId { get; set; }
+
+        public string PollItemKey { get; set; }
+
+        public string VotersComment { get; set; }
+
+        public string BlogPostPartitionKey { get; set; }
+
+        public string BlogPostRowKey { get; set; }
+    }
+}
