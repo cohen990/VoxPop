bdecdded393739da17748c06019808d769fa0c71
AJizzleBizzle <aaron_jerez@hotmail.com>
2015-05-01 14:40:00 +0100

    WIP

---
+++ VoxPop/Site/Storage/IResponseStore.cs
@@ -0,0 +1,18 @@
+﻿namespace Site.Storage
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Models;
+
+    public interface IResponseStore
+    {
+        IEnumerable<ResponseEntity> GetAllResponses(string blogRowKey);
+
+        Task CreateResponseAsync(ResponseEntity entity);
+
+        void MergeResponse(ResponseEntity entity);
+
+        ResponseEntity GetResponse(string entityRowKey, string entityPartitionKey);
+
+    }
+}
---
+++ VoxPop/Site/Storage/TableResponseStore.cs
@@ -0,0 +1,71 @@
+namespace Site.Storage
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.WindowsAzure;
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Table;
+    using Models;
+
+    public class TableResponseStore : IResponseStore
+    {
+        private readonly CloudTable _table;
+
+        public TableResponseStore()
+        {
+            CloudTableClient client = GetStorageAccount().CreateCloudTableClient();
+
+            _table = client.GetTableReference("voxpopresponses");
+            _table.CreateIfNotExists();
+        }
+
+        public IEnumerable<ResponseEntity> GetAllResponses(string blogRowKey)
+        {
+            var query = new TableQuery<ResponseEntity>();
+
+            IEnumerable<ResponseEntity> entities = _table.ExecuteQuery(query)
+                .Where(x => x.ReplyeeRowKey == blogRowKey)
+                .Select(x => x);
+
+            return entities;
+        }
+
+        public ResponseEntity GetResponse(string entityRowKey, string entityPartitionKey)
+        {
+            TableOperation operation = TableOperation.Retrieve<ResponseEntity>(entityPartitionKey, entityRowKey);
+
+            TableResult result = _table.Execute(operation);
+
+            var entity = result.Result as ResponseEntity;
+
+            return entity;
+        }
+
+        public async Task CreateResponseAsync(ResponseEntity entity)
+        {
+            var operation = TableOperation.Insert(entity);
+            await _table.ExecuteAsync(operation);
+        }
+
+        public void MergeResponse(ResponseEntity entity)
+        {
+            TableOperation operation = TableOperation.Merge(entity);
+
+            _table.Execute(operation);
+        }
+
+        private CloudStorageAccount GetStorageAccount()
+        {
+            string connectionString = CloudConfigurationManager.GetSetting("voxpop.responsestorage");
+
+            CloudStorageAccount storageAccount;
+            if (CloudStorageAccount.TryParse(connectionString, out storageAccount))
+                return storageAccount;
+
+            string message = string.Format("The connection string '{0}' is invalid.", connectionString);
+            throw new InvalidOperationException(message);
+        }
+    }
+}
