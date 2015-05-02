147ac3f709fd275da3dac9008ef89e0ccf1b679e
AJizzleBizzle <aaron_jerez@hotmail.com>
2015-05-02 03:27:07 +0100

    WIP

---
+++ VoxPop/Site/Models/BlogResponseViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Site.Models
+{
+    public class BlogResponseViewModel
+    {
+        public BlogModel Blog { get; set; }
+
+        public ResponseModel Response {get; set;}
+    }
+}
+
